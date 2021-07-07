48d3b1b8c8865e3f6b27e7bc303c804c30ff49b7
ortegaje <jeffersonj-o@hotmail.com>
2021-07-07 16:13:14 -0500

    new table custodian log

---
+++ database/migrations/2021_07_06_200119_create_custodian_log_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustodianLogTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('custodian_log', function (Blueprint $table) {
+            //$table->id();
+            $table->unsignedBigInteger('computer_id')->nullable();
+            $table->char('custodian_name', 56);
+            $table->char('position', 56)->nullable();
+            $table->char('location', 56);
+            $table->unsignedBigInteger('statu_id')->nullable();
+            $table->timestamp('assignment_date')->nullable();
+
+            $table->foreign('statu_id')->references('id')->on('status')->nullOnDelete()->cascadeOnUpdate();
+            $table->foreign('computer_id')->references('id')->on('computers')->nullOnDelete()->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('custodian_log');
+    }
+}
