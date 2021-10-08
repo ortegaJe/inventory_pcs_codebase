4abbd8a737d039ae85b1e6267bfbf6a54bc59452
ortegaje <jeffersonj-o@hotmail.com>
2021-10-08 15:08:27 -0500

    models report

---
+++ app/Models/Report.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Report extends Model
+{
+    use HasFactory;
+
+    const REPORT_REMOVE_NAME_ID = 1;
+}
